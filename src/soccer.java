64be1172c83ce2f24cabb8277019e9f03f6c0449
JohnnyB99 <s433561@ocsb.local>
2017-09-19 14:34:48 -0400

    Added story

---
+++ src/soccer.java
@@ -0,0 +1,17 @@
+
+public class soccer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Mr.Zebarth !");
+		System.out.println("I started playing soccer back in my country when I was young");
+		System.out.println("I was about 6 years old");
+		System.out.println("My older friends used to teach some skills");
+		System.out.println("I didn't join any club");
+		System.out.println("Now I am in high school");
+		System.out.println("I am trying out for soccer team");
+		System.out.println("I hope I make it");
+
+	}
+
+}
